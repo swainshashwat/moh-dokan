db92fece79bf26cdc98a6349181265b470f445f8
Shashwat Sourav Swain <shashwat.swain@reverieinc.com>
2022-03-27 21:37:43 +0530

    added registration

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,8 +1,12 @@
+import bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config["SECRET_KEY"] = "secretkeylol"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-from shop import routes
+from shop.admin import routes
--- shop/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, session, request, redirect, url_for
-
-from shop import app, db
-
-@app.route('/')
-def home():
-    return "HOME"
